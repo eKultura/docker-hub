9dcf36d424cef55e19c2e0b60ded6298f387b5ad
ionno <illnezz@centrum.cz>
2025-03-20 10:02:07 +0100

    added django_maria docker without documentation

---
+++ django_mariadb_elasticsearch/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# spustění SSH serveru
+service ssh start
+
+# Spuštění prvního skriptu - vytvoření uživatelů
+echo "Running users.sh..."
+./scripts/users.sh
+
+# Spustí collectstatic pro django
+cd div_app
+python manage.py collectstatic --noinput
+
+# Nastavení čekání, než naběhne mariadb
+echo "Čekání 15sec na start mariadb"
+sleep 15
+
+# Spustí Django přes `nohup`, aby běželo na pozadí a byly viditelné logy
+echo "Spouštím django server"
+nohup python manage.py runserver 0.0.0.0:8000 > django.log 2>&1 &
+
+# Přesměrování logů do výstupu, aby se docker nevypl, pokud vypneme nohup
+tail -f django.log
